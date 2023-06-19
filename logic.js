764037ecafa0e8bd7a65533332e6b3c1a44671ba
beckdeyoung816 <beckdeyoung@gmail.com>
2023-06-19 14:29:46 -0400

    Added Study Ability

--- logic.js
+++ logic.js
@@ -1,10 +1,10 @@
 const scotch = ['e4', 'e5', 'Nf3', 'Nc6', 'd4'];
-const vienna = ['e4', 'e5', 'Nf3', 'Nf6', 'c4'];
+const vienna = ['e4', 'e5', 'Nc6', 'Nf3', 'f4'];
 const kings_indian = ['e4', 'e6', 'd3', 'd5', 'Nd2'];
 let moveNum = 0;
 let moveNumIDVal = moveNum + 1;
 const checkButton = document.getElementById('check-button');
-let clicks = 0;
+clearButton = document.getElementById('clear-study-button');
 
 const opening = (openingType) => {
     
@@ -39,176 +39,104 @@ const clearNotationContent = () => {
     notationList.innerHTML = '';
 }
 
-const study1 = () => {
-    study(0);}
-
-const study = (n) => {
-    console.log("movenum=" + moveNum);
-    console.log("moveID=" + moveNumIDVal);
+const showNextStudyMove = () => {
+    console.log("Move #=" + moveNum);
+    console.log("Move ID #=" + moveNumIDVal);
     console.log("")
-    const openingType = document.getElementById('opening-options').value;
-    const notation = opening(openingType);
-    // const studyList = document.getElementById('study-list');
+
+    // get the study list items
     const studyListItems = document.getElementsByClassName('input-move')
 
     // if the moveNum is greater than the length of the notation, then the study is complete
-    if (moveNum >= notation.length) {
+    if (moveNum >= studyListItems.length) {
         console.log('study complete');
-        resetStudy();
+        // Add the clear button to reset the study
+        clearButton.style.display = 'flex';
         return;
     }
-
-    studyListItems[moveNum].style.display = 'flex';
-    guess = document.getElementById('input-move' + moveNumIDVal.toString());
+    
+    const curItem = studyListItems[moveNum]
+    curItem.style.display = 'flex';
+    curItem.style.width = '50px';
+    curItem.style.marginLeft = '0px';
     checkButton.style.display = 'flex';
-    checkButton.addEventListener('click', () => {
-            clicks++;
-            console.log("Num Clicks:" + clicks);
-            console.log("FUNCALL:" + n)
-            if (document.getElementById('correct-move-text') != null) {
-                document.getElementById('correct-move-text').remove();
-            }
-            if (guess.value != '') {
-                checkGuess(guess, notation[moveNum], n);
-            }
-        } 
-    )
 }
 
-// fun1(10)
-// fun1(n)
-//     if n == 1 return
-//     fun1(n-1)
-//     print(n)
+const checkGuess = () =>{
+    // remove the correct move text if it exists
+    if (document.getElementById('correct-move-text') != null) {
+        document.getElementById('correct-move-text').remove();
+    }
 
+    if (document.getElementById('empty-move-text') != null) {
+        document.getElementById('empty-move-text').remove();
+    }
 
-// fun2 
+    // get the guess and the answer
+    const openingType = document.getElementById('opening-options').value;
+    const notation = opening(openingType);
+    
+    const answer = notation[moveNum];
+    const guess = document.getElementById('input-move' + moveNumIDVal.toString());
 
-const checkGuess = (guess, answer, n) =>{
     console.log("Guess: " + guess.value)
     console.log("Answer: " + answer)
+
     // If the guess is correct, display the text as green
     // If the guess is incorrect, display the text as red and show the correct move next to it
     if (guess.value.toLowerCase() == answer.toLowerCase()) {
         console.log('correct');
         guess.style.color = 'green';
+        // increment the moveNum and moveNumIDVal
         moveNum++;
         moveNumIDVal++;
+        // call study again to show the next move
+        showNextStudyMove();
+    } else if (guess.value == '') {
+        console.log('empty');
+        const empty_message = document.createElement('p');
+        empty_message.innerHTML = `Please Enter A Move`;
+        empty_message.style.color = 'blue';
+        empty_message.id = 'empty-move-text';
+        // Paragraph elements have automatic padding, so we need to remove it
+        empty_message.style.marginBlockStart = '0px';
+        empty_message.style.marginBlockEnd = '0px';
+        document.getElementById('move-container' + moveNumIDVal.toString()).appendChild(empty_message);
+    
     } else {
         console.log('wrong');
         guess.style.color = 'red';
         const correct_move = document.createElement('p');
         correct_move.innerHTML = `Wrong: The correct move is ${answer}. Try again!`;
+        correct_move.style.color = 'red';
         correct_move.id = 'correct-move-text';
-        document.getElementById('study-list').appendChild(correct_move);
+        // Paragraph elements have automatic padding, so we need to remove it
+        correct_move.style.marginBlockStart = '0px';
+        correct_move.style.marginBlockEnd = '0px';
+        document.getElementById('move-container' + moveNumIDVal.toString()).appendChild(correct_move);
     }
     console.log("");
-    study(n + 1);
-}
-
-const resetStudy = () => {
-    moveNum = 0;
-    moveNumIDVal = moveNum + 1;
-
-    clearButton = document.getElementById('clear-study-button');
-    clearButton.style.display = 'flex';
-    clearButton.addEventListener('click', () => {
-        clearStudy();
-    })
 }
 
 const clearStudy = () => {
+    // reset the moveNum and moveNumIDVal
+    moveNum = 0;
+    moveNumIDVal = moveNum + 1;
+    // reset the input fields
     const studyListItems = document.getElementsByClassName('input-move');
-    studyListItems.forEach(element => {
-        element.style.display   = 'none';
-    })
-    Button.style.display = 'none';
+
+    // loop through the input fields and reset them
+    for (let i = 0; i < studyListItems.length; i++) {
+        studyListItems[i].style.display = 'none';
+        studyListItems[i].value = null;
+        studyListItems[i].placeholder = 'Move' + (i + 1);
+        studyListItems[i].style.color = 'black';
+    }
+
+    // hide the check and clear buttons
+    checkButton.style.display = 'none';
     clearButton.style.display = 'none';
 }
-
-
-// const study = () => {
-//     const openingType = document.getElementById('opening-options').value;
-//     const notation = opening(openingType);
-//     const studyList = document.getElementById('study-list');
-//     // studyList.style.display = 'flex';
-//     const checkButton = document.getElementById('check-button');
-//     checkButton.style.display = 'block';
-
-//     let currStudyMoveIndex = 0;
-
-//     // show the first move by un-hiding the first list item
-//     studyList.forEach(element => {
-//         element.style.display = 'block';})
-
-//     // studyList[currStudyMoveIndex].style.display = 'block';
-
-//     // When the check button is clicked, check the guess and show the next move
-//     checkButton.addEventListener('click', () => {
-//         const guess = document.getElementById('guess').value;
-//         const answer = notation[currStudyMoveIndex];
-//         checkGuess(guess, answer);
-//         currStudyMoveIndex = showNextStudyMove(currStudyMoveIndex, studyList);})
-// }
-
-// const showNextStudyMove = (currStudyMoveIndex, studyList) => {
-//     if (currStudyMoveIndex < studyList.length) {
-//         studyList[currStudyMoveIndex].classList.add('show-item');
-//         currStudyMoveIndex++;
-// }
-//     // return currStudyMoveIndex;
-//     return currStudyMoveIndex;
-// }
-// async function study(){
-//     const openingType = document.getElementById('opening-options').value;
-//     const notation = opening(openingType);
-    
-//     const study_list = document.getElementById('study-list');
-
-//     for (let i = 0; i < notation.length; i++) {
-
-//         // Create input text field for guessing the next move
-//         const input_guess = document.createElement('input');
-//         input_guess.type = 'text';
-//         input_guess.className = 'guess'
-//         input_guess.id = 'guess';
-//         input_guess.placeholder = 'Enter your guess';
-//         // Display the input field
-//         study_list.appendChild(input_guess);
-
-//         // Create a button to check the guess
-//         const check_button = document.createElement('button');
-//         check_button.id = 'check-button';
-//         check_button.innerHTML = 'Check';
-//         // Display the button
-//         study_list.appendChild(check_button);
-
-//         check_button.addEventListener('click', () => {
-//             const guess = document.getElementById('guess-${i}').value;
-//             const answer = notation[i];
-//             checkGuess(guess, answer);
-//         });
-        
-
-//         await new Promise(resolve => {
-//             const button = document.getElementById('checkButton');
-//             button.addEventListener('click', resolve, { once: true });
-//             });
-
-
-// const checkGuess = (guess, answer) =>{
-//     // If the guess is correct, display the text as green
-//     // If the guess is incorrect, display the text as red and show the correct move next to it
-//     if (guess == answer) {
-//         document.getElementById('guess').style.color = 'green';
-//     } else {
-//         document.getElementById('guess').style.color = 'red';
-//         const correct_move = document.createElement('p');
-//         correct_move.innerHTML = `Wrong: The correct move is ${answer}`;
-//         document.getElementById('study-list').appendChild(correct_move);
-//     }
-
-// }
 
 // Make an 8x8 grid
 const makeGrid = () => {
@@ -229,4 +157,4 @@ const makeGrid = () => {
 }
 
 
-// makeGrid();
+makeGrid();
